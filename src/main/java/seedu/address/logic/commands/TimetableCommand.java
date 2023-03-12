15d6ee0946abcb27cfe4f96ecc223e37c13c16c7
dohaduong <duonghado03@gmail.com>
2023-03-12 13:16:21 +0800

    Add timetable button Timetable button helps to open a new window to display timetable (jobs sorted based on timing) and view scheduled jobs.
    A separate menu will demarcate contact management and job scheduling functions of the app.
    
    Let's replicate the main window after selecting the job item in the job menu
    
    It is done this way so we can still follow and extend from the existing code structure.

---
+++ src/main/java/seedu/address/logic/commands/TimetableCommand.java
@@ -0,0 +1,21 @@
+package seedu.address.logic.commands;
+
+import seedu.address.model.Model;
+
+import static java.util.Objects.requireNonNull;
+
+public class TimetableCommand extends Command {
+    public static final String COMMAND_WORD = "timetable";
+
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Shows timetable and job schedule.\n"
+            + "Example: " + COMMAND_WORD;
+
+    public static final String SHOWING_TIMETABLE_MESSAGE = "Opened timetable window.";
+
+    @Override
+    public CommandResult execute(Model model) {
+        requireNonNull(model);
+        return new CommandResult(SHOWING_TIMETABLE_MESSAGE, true, false);
+    }
+
+}
